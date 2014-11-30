c0e15882c1f5280f28d9ce004a4947f16d6d0ef0
MichalMeszaros <meszaros.michal1@gmail.com>
2014-11-30 12:48:17 +0100

    Zaverecny projekt aj s dokumentaciou, unit testy su v projekte wikiparser

---
+++ Codes/CanTheyMeet/CanTheyMeet/Date.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CanTheyMeet
+{
+    class Date
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public Date(string data)
+        {
+            if (!data.Equals("NOTSET"))
+            {
+                try
+                {
+                    Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+                    string[] pole = data.Split('|');
+                    Year = int.Parse(rgx.Replace(pole[0], ""));
+                    Month = int.Parse(rgx.Replace(pole[1], ""));
+                    Day = int.Parse(rgx.Replace(pole[2], ""));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Something wen wrong  date was --> " + data + "\n Exception is " + e.Message);
+                    Year = 0;
+                    Month = 0;
+                    Day = 0;
+                }
+            }
+            else
+            {
+                Year = 0;
+                Month = 0;
+                Day = 0;
+            }
+        }
+        public override string ToString()
+        {
+            return "   "+Year+" | "+Month+" | "+Day;
+        }
+    }
+}
