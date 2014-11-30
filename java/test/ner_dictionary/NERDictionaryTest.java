9fd19f3797b00c663f215f106c400fbcdd3f7997
Andrej Valko <valkoandrej.sk@gmail.com>
2014-11-30 16:00:14 +0100

    Tests corrected.

--- java/test/ner_dictionary/NERDictionaryTest.java
+++ java/test/ner_dictionary/NERDictionaryTest.java
@@ -22,6 +22,9 @@ import org.junit.rules.TemporaryFolder;
 public class NERDictionaryTest {
 
 	private static NERDictionary app;
+	private static final String INPUT_FILE = "sample_alts_enwiki-latest-pages-articles1.xml";
+	private static final String INPUT_RULES_FILE = "Category_recognition_rules.xml";
+	private static final String OUTPUT_FILE = "sample_output_NER_dictionary.tsv";
 	
 	@org.junit.Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
@@ -34,15 +37,21 @@ public class NERDictionaryTest {
 	@Test
 	public void testParseRules() throws IOException {
 		String categoryMappingFilePath = testFolder.newFile().getPath();
-		URL url = getClass().getResource("resources/Category_recognition_rules.xml");
+		URL url = this.getClass().getClassLoader().getResource(INPUT_RULES_FILE);;
 		String rulesFilePath = url.getPath();
 		RuleSet ruleSet = app.parseRules(rulesFilePath, categoryMappingFilePath);
-		Rule rule = ruleSet.getRuleList().get(0);
 		
-		assertEquals("(?m)^\\s*\\|\\s*birth_", rule.getPattern());
-		assertEquals("Person", rule.getCategoryName());
-		assertTrue(rule.getCategoryId() == 1);
-		assertEquals("none", rule.getTypeAsString());
+		assertFalse(ruleSet.getRuleList().isEmpty());
+		
+		for (Rule rule : ruleSet.getRuleList()) {
+			assertNotNull(rule.getPattern());
+			assertNotEquals(rule.getPattern(), "");
+			assertNotNull(rule.getCategoryName());
+			assertNotEquals(rule.getCategoryName(), "");
+			assertNotNull(rule.getTypeAsString());
+			assertNotEquals(rule.getTypeAsString(), "");
+			assertNotNull(rule.getCategoryId());
+		}
 	}
 
 	@Test
@@ -50,23 +59,29 @@ public class NERDictionaryTest {
 		File indexDirectory = testFolder.newFolder();
 		Indexer indexer = new Indexer(indexDirectory.getPath());
 		
-		URL sourceUrl = getClass().getResource("resources/sample_alts_enwiki-latest-pages-articles1.xml");
-		String sourceFileName = URLDecoder.decode(sourceUrl.getPath(), StandardCharsets.UTF_8.displayName());
+		URL sourceUrl = this.getClass().getClassLoader().getResource(INPUT_FILE);
+		String sourceFilePath = URLDecoder.decode(sourceUrl.getPath(), StandardCharsets.UTF_8.displayName());
 		List<String> sourceList = new ArrayList<String>();
-		sourceList.add(sourceFileName);
+		sourceList.add(sourceFilePath);
 		
 		String categoryMappingFilePath = testFolder.newFile().getPath();
-		URL rulesUrl = getClass().getResource("resources/Category_recognition_rules.xml");
-		String rulesFilePath = rulesUrl.getPath();
+		
+		URL rulesUrl = this.getClass().getClassLoader().getResource(INPUT_RULES_FILE);
+		String rulesFilePath = URLDecoder.decode(rulesUrl.getPath(), StandardCharsets.UTF_8.displayName());
 		RuleSet ruleSet = app.parseRules(rulesFilePath, categoryMappingFilePath);
 		
 		File outputFile = testFolder.newFile();
 		
 		app.parseAndIndexWikipedia(sourceList, outputFile.getPath(), ruleSet, indexer);
+		List<String> outputLines = Files.readAllLines(outputFile.toPath(), StandardCharsets.UTF_8);
 		
-		List<String> lines = Files.readAllLines(outputFile.toPath(), StandardCharsets.UTF_8);
-		assertTrue(lines.size() == 1);
-		assertEquals("Abraham Lincoln" + WikiXMLParserHandler.SEPARATOR + "1", lines.get(0));
+		URL expectedOutputUrl = this.getClass().getClassLoader().getResource(OUTPUT_FILE);
+		String expectedOutputFilePath = URLDecoder.decode(expectedOutputUrl.getPath(), StandardCharsets.UTF_8.displayName());
+		File expectedOutputFile = new File(expectedOutputFilePath);
+		List<String> expectedLines = Files.readAllLines(expectedOutputFile.toPath(), StandardCharsets.UTF_8);
+		
+		for (int i = 0; i < expectedLines.size(); i++) {
+			assertEquals(expectedLines.get(i), outputLines.get(i));
+		}
 	}
-
 }
